aec8f73db5f60aedac06dac0eecc35ec2c2b7885
bssthu <bssthu@users.noreply.github.com>
2016-06-17 23:13:36 +0800

    add python2 support

--- pysqlsimplecipher/util.py
+++ pysqlsimplecipher/util.py
@@ -12,6 +12,7 @@ import os
 import math
 import hashlib
 import hmac
+import struct
 from Crypto.Cipher import AES
 
 
@@ -31,18 +32,26 @@ def is_valid_database_header(header):
 
 def is_valid_decrypted_header(header):
     # skip first 16 bytes
+    if type(header) is str:     # python2
+        header = [ord(x) for x in header]
     return header[21-16] == 64 and header[22-16] == 32 and header[23-16] == 32
 
 
 def get_page_size_from_database_header(header):
-    page_sz = int.from_bytes(header[16:18], 'big')
+    if type(header) is str:     # python2
+        page_sz = 256 * ord(header[16]) + ord(header[17])
+    else:
+        page_sz = int.from_bytes(header[16:18], 'big')
     if page_sz == 1:        # since SQLite version 3.7.1
         page_sz = 65536
     return page_sz
 
 
 def get_reserved_size_from_database_header(header):
-    return int(header[20])
+    if type(header) is str:     # python2
+        return ord(header[20])
+    else:
+        return int(header[20])
 
 
 def is_valid_page_size(page_sz):
@@ -62,13 +71,18 @@ def key_derive(salt, password, salt_mask, key_sz, key_iter, hmac_key_sz, hmac_ke
     """Derive an encryption key for page encryption/decryption, an key for hmac generation"""
     key = hashlib.pbkdf2_hmac('sha1', password, salt, key_iter, key_sz)
 
-    hmac_salt = bytearray([x ^ salt_mask for x in salt])
-    hmac_key = hashlib.pbkdf2_hmac('sha1', key, hmac_salt, hmac_key_iter, hmac_key_sz)
-
+    try:
+        hmac_salt = bytearray([x ^ salt_mask for x in salt])
+        hmac_key = hashlib.pbkdf2_hmac('sha1', key, hmac_salt, hmac_key_iter, hmac_key_sz)
+    except TypeError:   # python2
+        hmac_salt = b''
+        for x in salt:
+            hmac_salt += chr(ord(x) ^ salt_mask)
+        hmac_key = hashlib.pbkdf2_hmac('sha1', str(key), hmac_salt, hmac_key_iter, hmac_key_sz)
     return key, hmac_key
 
 
 def generate_hmac(hmac_key, content, page_no):
     hmac_obj = hmac.new(hmac_key, content, hashlib.sha1)
-    hmac_obj.update(page_no.to_bytes(4, 'little'))
+    hmac_obj.update(struct.pack('<i', page_no))
     return hmac_obj.digest()
